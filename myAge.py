368ed315694344a6b8eeed32d18a192bb8195d7d
felipe-97 <lopezenriquezluisfelipe@gmail.com>
2020-08-27 12:18:07 -0500

    primer script de python

---
+++ myAge.py
@@ -0,0 +1,13 @@
+#Developer: luis felipe lopez
+
+'''  what does scripts de?:
+este script muestra tu nombra y tu edad
+
+comentario de bloque:
+'''
+myName = 'luis felipe lopez'
+myDate = 1997
+year = 2020
+age= year - myDate
+
+print ("your name is: ", myName, "your are", age,"year old") 
